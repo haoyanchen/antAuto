2d0cb43fdd9075352aa468777ab8c85616a15139
haoyanchen <1411348474@qq.com>
2022-01-03 18:23:22 +0800

    PVP

--- airtestRuAuto/aaa.py
+++ airtestRuAuto/aaa.py
@@ -1,6 +1,11 @@
-# from airtest.core.api import *
-# from airtest.cli.parser import cli_setup
-# import os
+from airtest.core.api import *
+from airtest.cli.parser import cli_setup
+import os
+from PIL import Image
+import pytesseract
+
+
+
 #
 # if not cli_setup():
 #     auto_setup(__file__, logdir='./reuse/log')
@@ -10,3 +15,12 @@
 #     print("aaaaaaaaaa")
 #     print(os.getcwd())
 #     touch(Template(os.getcwd()+"\\reuse\\img\\tpl1637587933540.png", record_pos=(0.102, 0.192), resolution=(2280, 1080)))
+
+# print(os.getcwd())
+image = Image.open(os.getcwd()+"\\reuse\\img\\tpl1637521250730.png")
+image1 = Image.open(os.getcwd()+"\\reuse\\img\\tpl1637488379134.png")
+text = pytesseract.image_to_string(image, lang='chi_sim', config="--psm 6")
+text1 = pytesseract.image_to_string(image1, lang='chi_sim', config="--psm 6")
+print("结果如下：")
+print(text)
+print(text1)
--- airtestRuAuto/reuse/common.py
+++ airtestRuAuto/reuse/common.py
@@ -2,12 +2,21 @@ import datetime
 
 from airtest.core.api import *
 from airtest.cli.parser import cli_setup
+from PIL import Image
+import pytesseract
 
 if not cli_setup():
     auto_setup(__file__, logdir='./reuse/log')
 
 
-def nowTime():
-    nowTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    return nowTime
+# 获取当前时间
+def now_time():
+    now_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return now_time
 
+
+# 文字识别(需要安装Tesseract4)
+def orc_image(file):
+    image = Image.open(file)
+    text = pytesseract.image_to_string(image, lang='chi_sim', config="--psm 6")
+    return text
